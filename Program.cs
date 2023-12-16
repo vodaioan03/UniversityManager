4fca2c037ea0686d382ad4585a1a60b50f29f609
Voda Ioan <vodaioan03@gmail.com>
2023-12-17 01:44:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Provide a menu-driven console-based user interface. Implementation details are up to you
+    Employ layered architecture and classes
+    Have at least 20 procedurally generated items for each domain class in your application at startup (e.g., at least 20 students, 20 disciplines and 20 grades)
+    Provide specifications and PyUnit test cases for all non-UI classes and methods for the first functionality
+    Implement and use your own exception classes.
+    Deadline for maximum grade is week 11.
+ * 
+ *  Students Register Management
+        A faculty stores information about:
+
+        Student: student_id, name
+        Discipline: discipline_id, name
+        Grade: discipline_id, student_id, grade_value
+        Create an application to:
+
+        Manage students and disciplines. The user can add, remove, update, and list both students and disciplines.
+        Grade students at a given discipline. Any student may receive one or several grades at any discipline. Deleting a student also removes their grades. Deleting a discipline deletes all grades at that discipline for all students.
+        Search for disciplines/students based on ID or name/title. The search must work using case-insensitive, partial string matching, and must return all matching disciplines/students.
+        Create statistics:
+        All students failing at one or more disciplines (students having an average <5 for a discipline are failing it)
+        Students with the best school situation, sorted in descending order of their aggregated average (the average between their average grades per discipline)
+        All disciplines at which there is at least one grade, sorted in descending order of the average grade(s) received by all students
+ * 
+ */
+using System;
+
+namespace UniversityManager
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            uiMenu menu = new uiMenu();
+            menu.StartMenu();
+        }
+
+    }
+
+}
---
+++ uiMenu.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityManager
+{
+    public class uiMenu
+    {
+        LogicClass logicClass;
+        inputMenu inputmenu;
+        public bool programRunning = false;
+
+        public uiMenu() 
+        {
+            this.logicClass = new LogicClass();
+            this.programRunning = true;
+            this.inputmenu = new inputMenu(this);
+        }
+
+        public void StartMenu()
+        {
+            while (programRunning)
+            {
+                mainMenu();
+                try
+                {
+                    inputmenu.inputOption();
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Input option wrong! Type an integer number.");
+                }
+            }
+        }
+
+        public void mainMenu()
+        {
+            Console.WriteLine(" == Menu University Management == ");
+            Console.WriteLine("1. Manage Students and Disciplines");
+            Console.WriteLine("2. Add Grade");
+            Console.WriteLine("3. Search Discipline");
+            Console.WriteLine("4. Search Student");
+            Console.WriteLine("5. Create Statistics");
+            Console.WriteLine("6. Exit");
+        }
+
+        public void manageMenu()
+        {
+            Console.WriteLine(" == Menu Manage Students and Disciplines ==");
+            Console.WriteLine("1. Add Student");
+            Console.WriteLine("2. Add Discipline");
+            Console.WriteLine("3. Delete Student");
+            Console.WriteLine("4. Delete Discipline");
+            Console.WriteLine("5. Update Student");
+            Console.WriteLine("6. Update Discipline");
+            Console.WriteLine("7. Show list with students.");
+            Console.WriteLine("8. Show list with disciplines");
+            Console.WriteLine("9. Back to Main Menu");
+            Console.WriteLine("10. Exit");
+        }
+
+    }
+
+    public class inputMenu
+    {
+        uiMenu menu;
+        OptionLogic logicoption;
+        int actualmenu;
+        public inputMenu(uiMenu menu)
+        {
+            this.logicoption = new OptionLogic(menu,this);
+            this.menu = menu;
+            this.actualmenu = 0;
+        }
+
+        public void inputOption()
+        {
+            Console.Write("Type option: ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            this.actualmenu++;
+            try
+            {
+                this.logicoption.ConvertOption(option, this.actualmenu);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            this.actualmenu--;
+        }
+    }
+
+    public class OptionLogic
+    {
+        Action[][] actionsMenu;
+        uiMenu menu;
+        inputMenu inputmenu;
+        public OptionLogic(uiMenu menu, inputMenu imenu)
+        {
+            this.actionsMenu = new Action[][] { new Action[] {() => ManageMenuLogic(), () => AddGrade(), () => SearchDiscipline(), () => SearchStudent(), () => CreateStatistic(), () => ExitMenu() },
+                                                new Action[] {() => AddStudent(), () => AddDiscipline(), () => RemoveStudent(), () => RemoveDiscipline(), () => ModifyStudent(), () => ModifyDiscipline(), () => ListStudent(), () => ListDiscipline(), () => BackMenu(), () => ExitMenu() } };
+            this.inputmenu = imenu;
+            this.menu = menu;
+        }
+
+        public void ConvertOption(int option, int actualMenu)
+        {
+            Console.WriteLine("In option convert");
+            option--;
+            actualMenu--;
+            if (actualMenu < this.actionsMenu.Length && option < this.actionsMenu[actualMenu].Length)
+            {
+                this.actionsMenu[actualMenu][option]();
+            }
+            else
+            { }
+        }
+
+        public void ManageMenuLogic()
+        {
+            this.menu.manageMenu();
+            this.inputmenu.inputOption();
+        }
+
+        public void AddGrade()
+        {
+
+        }
+
+        public void SearchDiscipline()
+        {
+
+        }
+
+        public void SearchStudent()
+        {
+
+        }
+        public void CreateStatistic()
+        {
+
+        }
+        public void ExitMenu()
+        {
+            Console.WriteLine("Exiting...");
+            this.menu.programRunning = false;
+        }
+
+        //FOR Manage Menu
+
+        public void AddStudent()
+        {
+            Console.WriteLine("inaddStudent");
+        }
+        public void AddDiscipline()
+        {
+            Console.WriteLine("In Add Discipline");
+        }
+        public void RemoveStudent()
+        {
+            Console.WriteLine("in Remove Student");
+        }
+        public void RemoveDiscipline()
+        {
+            Console.WriteLine("In remove Discipline");
+        }
+        public void ModifyStudent()
+        {
+            Console.WriteLine("In Modify Student");
+        }
+        public void ModifyDiscipline()
+        {
+            Console.WriteLine("In Modify Discipline");
+        }
+        public void ListStudent()
+        {
+            Console.WriteLine("In list Student");
+        }
+        public void ListDiscipline()
+        {
+            Console.WriteLine("In List discipline");
+        }
+        public void BackMenu()
+        {
+            Console.WriteLine("In Back Menu");
+        }
+
+    }
+}
